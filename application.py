572ddc46d5d4a4b8d482215dff31f198bff73412
Yulei Li <liyulei2018@gmail.com>
2020-07-11 00:37:24 +0200

    :tada: Inital commit.

---
+++ application.py
@@ -0,0 +1,40 @@
+# Import Python Packages
+import numpy as np
+from functions import solve_farmer_model
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+# Set parameter values.
+beta = 0.95
+alpha = 0.3
+delta = 0.1
+rho = 0.9
+
+# Set the number of time periods to simulate.
+T = 200
+
+# first 10 periods given
+inital_productivity_state = [0, -0.005, -0.009,
+                             -0.013, -0.022, -0.021, -0.019, -0.011, -0.012, -0.003]
+
+# Get simulation result.
+s_t, c_t, k_t, y_t = solve_farmer_model(alpha, beta, delta, rho, T, inital_productivity_state)
+
+# plot time series.
+time_1 = np.array(list(range(T)))
+time_2 = np.array(list(range(T + 1)))
+
+sns.set_style("whitegrid")
+fig, ax = plt.subplots(figsize=(T / 8, T / 16))
+
+# Consumption levels, capital levels, and output levels against time.
+ax.plot(time_1, s_t, label="productivity state", color="C0")
+ax.plot(time_1, c_t, label="consumption", color="C1")
+ax.plot(time_2, k_t, label="capital", color="C2")
+ax.plot(time_2, k_t, label="output", color="C3")
+ax.legend(loc="upper left", fontsize=T / 10)
+
+plt.xlabel('Time', fontsize=T / 6)
+plt.title('Productivity state, Consumption, capital and output over time', fontsize=T / 5)
+fig.savefig("figures/time_series_plot")
