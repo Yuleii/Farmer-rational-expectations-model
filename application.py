c0292a19f97f9032bd21496704d1a180c266c793
Yulei Li <liyulei2018@gmail.com>
2020-07-11 11:11:47 +0200

    :art: Improve plot.

--- application.py
+++ application.py
@@ -29,10 +29,10 @@ sns.set_style("whitegrid")
 fig, ax = plt.subplots(figsize=(T / 8, T / 16))
 
 # Consumption levels, capital levels, and output levels against time.
-ax.plot(time_1, s_t, label="productivity state", color="C0")
-ax.plot(time_1, c_t, label="consumption", color="C1")
-ax.plot(time_2, k_t, label="capital", color="C2")
-ax.plot(time_1, y_t, label="output", color="C3")
+ax.plot(time_1, s_t, label="productivity state", color="C7", alpha=0.7, linestyle='dashdot')
+ax.plot(time_1, c_t, label="consumption", color="C1", alpha=0.7, linestyle='dotted')
+ax.plot(time_2, k_t, label="capital", color="C2", alpha=0.7, linestyle='dashed')
+ax.plot(time_1, y_t, label="output", color="C0", alpha=0.7)
 ax.legend(loc="upper left", fontsize=T / 10)
 
 plt.xlabel("Time", fontsize=T / 6)
