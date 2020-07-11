4d6a6dd5462de621a7a3e22ca5a90dee812da3b8
Yulei Li <liyulei2018@gmail.com>
2020-07-11 13:31:26 +0200

    :pencil: Add comments.

--- application.py
+++ application.py
@@ -28,7 +28,7 @@ time_2 = np.array(list(range(T + 1)))
 sns.set_style("whitegrid")
 fig, ax = plt.subplots(figsize=(T / 8, T / 16))
 
-# Consumption levels, capital levels, and output levels against time.
+# Productivity state, consumption, capital and output against time.
 ax.plot(time_1, s_t, label="productivity state", color="C7", alpha=0.7, linestyle='dashdot')
 ax.plot(time_1, c_t, label="consumption", color="C1", alpha=0.7, linestyle='dotted')
 ax.plot(time_2, k_t, label="capital", color="C2", alpha=0.7, linestyle='dashed')
